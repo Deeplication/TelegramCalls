49d680aa6d0b2f652333a33218b7b5787699bf4b
Deeplication <138996913+Deeplication@users.noreply.github.com>
2025-08-10 15:45:43 +0300

    Update manifest.js

--- plugins/TelegramCalls/manifest.js
+++ plugins/TelegramCalls/manifest.js
@@ -4,7 +4,7 @@
   "description": "Mimic Telegram call audio: stereo media, no attenuation, MODE_NORMAL on wired, relaxed mic DSP.",
   "version": "1.0.0",
   "authors": [{ "name": "Deeplication" }],
-  "main": "index.ts",
+  "main": "src/index.ts",
   "license": "MIT",
   "source": "local"
 }
