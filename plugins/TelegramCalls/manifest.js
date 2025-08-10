86206e990c1641c3ebeaf06d4dc246c14c193630
Deeplication <138996913+Deeplication@users.noreply.github.com>
2025-08-10 14:25:03 +0300

    Create manifest.js

---
+++ plugins/TelegramCalls/manifest.js
@@ -0,0 +1,10 @@
+{
+  "id": "kettu.telegram-calls",
+  "name": "TelegramCalls",
+  "description": "Mimic Telegram call audio: stereo media, no attenuation, MODE_NORMAL on wired, relaxed mic DSP.",
+  "version": "1.0.0",
+  "authors": [{ "name": "Deeplication" }],
+  "main": "index.ts",
+  "license": "MIT",
+  "source": "local"
+}
